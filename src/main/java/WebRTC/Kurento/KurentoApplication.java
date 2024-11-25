9ef24be1325e387c78370ba2c65b3702b612c4ca
SuminSSon <itoodo12@gmail.com>
2024-11-25 20:51:34 +0900

    initial commit

---
+++ src/main/java/WebRTC/Kurento/KurentoApplication.java
@@ -0,0 +1,13 @@
+package WebRTC.Kurento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurentoApplication.class, args);
+	}
+
+}
